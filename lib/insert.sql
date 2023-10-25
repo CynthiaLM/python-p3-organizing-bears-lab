ee6c8b88c7b37f3dda61d7e067325981be5cb58c
Laleti Cynthia <+>
2023-10-25 09:28:45 +0300

    SQL lab submission

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Mr.Chocolate',2,'M','yellow',NULL,True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Rowdy',2,'M','orange',NULL,True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Tabitha',5,'F','pink',NULL,False);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Sergeant Brown',7,'M','yellow',NULL,False);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Melissa',1,'F','orange',NULL,True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Grinch',7,'M','red',NULL,False);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Wendy',10,'F','pink',NULL,True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,5,'M','green',NULL,False);
+
